4f05c6aa84a5e7b74734da8447f1a84ad33b37e0
Joao Pedro <mendoncajoaopedro441@gmail.com>
2023-11-22 12:11:02 -0300

    improv(user): find user by email

--- src/main/java/api/scrum/user/repository/UserRepository.java
+++ src/main/java/api/scrum/user/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package api.scrum.user.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,7 @@ import api.scrum.user.model.User;
 
 
 public interface UserRepository extends JpaRepository<User, UUID>{
+
+    Optional<User> findByEmail(String email);
 
 }
